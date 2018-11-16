bedb96fe1ede078d7552548dd57aa8619dd2eb8b
Thien LH <hongthien0110@gmail.com>
2018-11-16 11:29:16 +0700

    ~~~

--- template/head.php
+++ template/head.php
@@ -4,9 +4,9 @@
 <meta name="keywords" content="เรียนภาษาอังกฤษ,เก่งภาษาอังกฤษ,เรียนภาษาอังกฤษเบื้องต้น,เรียนภาษาอังกฤษเริ่มต้น,ภาษาอังกฤษเพื่อการสื่อสาร,สนทนาภาษาอังกฤษ,คอร์สเรียนภาษาอังกฤษ,คุยกับฝรั่งฝึก,ภาษาอังกฤษ,หาเพื่อนต่างชาติฝึกภาษาอังกฤษ,เรียนภาษาอังกฤษผ่านเน็ต,เรียนภาษาอังกฤษผ่านเว็บ,ภาษาอังกฤษในชีวิตประจําวัน,เรียนรู้ภาษาอังกฤษ,ฟังภาษาอังกฤษ,เรียนภาษาอังกฤษ online,เรียนภาษาอังกฤษออนไลน์,ฝึกภาษาอังกฤษ,เรียนภาษาอังกฤษด้วยตัวเอง,เรียนภาษาอังกฤษด้วยตนเอง,พูดภาษาอังกฤษ,ทดสอบภาษาอังกฤษ,แบบทดสอบภาษาอังกฤษออนไลน์,สอนภาษาอังกฤษออนไลน์,topica native,topica,วิธีพูดภาษาอังกฤษให้เก่ง,learn english,speak english,conversation,communication,teach english,listen english,test english,english online,อาจารย์เจ้าของภาษา"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<meta property="og:image" content="http://www.topicanative.asia/img/favicon.png" />
+<meta property="og:image" content="http://www.topicanative.asia/img/thumbnail.png" />
 <meta property="og:title" content="Topica Native" />
-<meta property="og:description" content="Learn English Online !!!" />
+<meta property="og:description" content="ภาษาอังกฤษออนไลน์สำหรับวัยทำงาน,เรียนภาษาอังกฤษ,ครูฝรั่งสอน,ฝึกพูดกับครูชาวต่างชาติ,เก่งภาษาอังกฤษ,คอมพิวเตอร์,ภาษาอังกฤษผ่านเน็ต,ฟังภาษาอังกฤษ,ฝึกภาษาอังกฤษ" />
 
 <link type="image/x-icon" href="img/favicon.png" rel="shortcut icon">
 <link rel="stylesheet" type="text/css" href="css/style-hover.css">
